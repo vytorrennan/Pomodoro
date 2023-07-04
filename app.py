6d1c4aca4248f529e825f44c627e6e10675629d5
vytorrennan <crosdeadbr@gmail.com>
2023-07-04 16:40:09 -0300

    Criado banco de dados, criado prototipo de tela de login e registro

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+basedir = path.abspath(path.dirname(__file__))
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + path.join(basedir, "database.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Student {self.email}>"
+
 
 @app.route("/")
 def pomodoro():
     return render_template("pomodoro.html")
 
-@app.route("/account")
+@app.route("/account", methods=["GET", "POST"])
 def account():
+    if request.method == "POST":
+        if request.form.get("btnRegister") == "Register":
+            newUser = Student(email=request.form["emailRegister"], password=request.form["passwordRegister"])
+            print(newUser)
+            print(newUser.id)
+            print(newUser.email)
+            print(newUser.password)
+        elif request.form.get("btnLogin") == "Login":
+            print("Login")
+        
     return render_template("account.html")
 
 if __name__ == "__main__":
--- static/js/account.js
+++ static/js/account.js
@@ -1,3 +1,14 @@
-document.querySelector("#login > a").addEventListener("click", () => {
-    
+const loginElement = document.querySelector("#login");
+const registerElement = document.querySelector("#register");
+
+document.querySelector("#register > a").addEventListener("click", (event) => {
+    event.preventDefault();
+    loginElement.style.display = "block";
+    registerElement.style.display = "none";
+})
+
+document.querySelector("#login > a").addEventListener("click", (event) => {
+    event.preventDefault();
+    registerElement.style.display = "block";
+    loginElement.style.display = "none";
 })
